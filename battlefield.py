72c74be761adf9dc92dbc871880859b2a72fcde3
Waltonr <waltonr18126@gmail.com>
2022-04-04 14:48:47 -0400

    completed run_game method and completed Battlefield class

--- battlefield.py
+++ battlefield.py
@@ -7,7 +7,9 @@ class Battlefield:
         self.dinosaur = Dinosaur('Godzilla', 25)
     
     def run_game(self):
-        pass
+        self.display_welcome()
+        self.battle_phase()
+        self.display_winner()
 
     def display_welcome(self):
         print('Welcome to Robots vs Dinosaurs!')
