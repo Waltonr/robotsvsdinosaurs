23891833e8b841fe443f22fbdbdc98230d0712a8
Waltonr <waltonr18126@gmail.com>
2022-04-04 12:43:32 -0400

    complete attack method in Robot class

--- robot.py
+++ robot.py
@@ -1,10 +1,16 @@
 from weapon import Weapon
 from dinosaur import Dinosaur
+
 class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.active_weapon = Weapon
+        self.active_weapon = Weapon('Gun', 30)
     
     def attack(self, dinosaur):
-        pass 
+        dinosaur = Dinosaur
+        weapon = Weapon
+        print(f'{self.name} attacks {dinosaur.name} with {weapon.name}')
+        dinosaur.health -= weapon.attack_power
+        print(dinosaur.health)
+ 
